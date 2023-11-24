876d21fdd7ee7e72e2a36be0e7aa249e1433d378
Soviet Ligal <soviet@Soviets-MacBook-Pro-2.local>
2023-11-24 16:05:17 +1100

    Added comments

--- public/index.php
+++ public/index.php
@@ -2,11 +2,18 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+/**
+ * The command sequence: "N E S W" will move the robot in a full square, returning it to where it started.
+ */
 $robot = \App\Service\RobotFactory::build();
 $robot->processCommands("N E S W");
 echo "Final Position: " . implode(', ', $robot->getCurrentPosition()) . "\n";
 echo "<br>";
 
+/**
+ * If the robot starts in the south-west corner of the warehouse then the following commands will move it to the middle of the warehouse.
+ * "N E N E N E N E"
+ * */
 $robot = \App\Service\RobotFactory::build();
 $robot->processCommands("N E N E N E N E");
 echo "Final Position: " . implode(', ', $robot->getCurrentPosition()) . "\n";
