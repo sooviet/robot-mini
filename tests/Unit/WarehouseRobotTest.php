682cbfa601fd2f5377a8c60ecb798f5394202896
Soviet Ligal <soviet@Soviets-MacBook-Pro-2.local>
2023-11-24 16:05:43 +1100

    Clear redundant code

--- tests/Unit/WarehouseRobotTest.php
+++ tests/Unit/WarehouseRobotTest.php
@@ -14,23 +14,6 @@ class WarehouseRobotTest extends TestCase
         $this->assertEquals([0, 0], $robot->getCurrentPosition());
     }
 
-    public function testProcessCommands(): void
-    {
-        $robot = new WarehouseRobot();
-
-        // Test a sequence of commands
-        $robot->processCommands('N E S W');
-        $this->assertEquals([0, 0], $robot->getCurrentPosition());
-
-        // Test another sequence of commands
-        $robot->processCommands('N E N E N E N E');
-        $this->assertEquals([4, 4], $robot->getCurrentPosition());
-
-        // Test an invalid command within the sequence
-        $this->expectException(InvalidCommandException::class);
-        $robot->processCommands('N X S');
-    }
-
     public function testMoveNorth(): void
     {
         $robot = new WarehouseRobot();
